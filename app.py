de985deee491d968e181c1c99966b68c9ec00ad6
tanvi <69223960+tanvividyala@users.noreply.github.com>
2025-04-06 09:55:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ import zipfile
 import tempfile
 
 st.set_page_config(page_title="Jean-O-Type 👖🦖", layout="centered")
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #FDFDFB;
+    }
+    .stApp {
+        background-color: #FDFDFB;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.markdown("<h1 style='text-align: center;'>JEAN-O-TYPE CLASSIFIER</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Upload a photo of jeans to discover your denim destiny!</p>", unsafe_allow_html=True)
 
